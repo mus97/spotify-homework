02a313a1d3e3ad99195862aa0652cb82dfc13cec
Maria Kuchinskaya <vretun@gmail.com>
2022-05-30 20:08:50 +0300

    add project spotify-homework

--- src/test/java/GetArticles.java
+++ src/test/java/GetArticles.java
@@ -1,6 +1,8 @@
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,12 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GetArticles {
+    private static final Logger logger = LogManager.getLogger();
 
     @Test
     public void testGetArticles() throws IOException {
-        HttpGet request = new HttpGet("https://api.reverb.com/api/articles");
+        HttpGet request = new HttpGet("https://api.spotify.com/v1/me");
         CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
-        assertEquals(200, response.getStatusLine().getStatusCode());
+        String body = response.toString();
+        logger.info(body);
+        System.out.println(body);
     }
 }
 
